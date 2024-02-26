b003092a65114d6d7b89eed494a00f92a4389888
Kirat Sidhu <kirat.sidhu.sw@gmail.com>
2024-02-25 20:58:39 -0800

    Format files

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -1,28 +1,28 @@
 return {
-	"nvim-treesitter/nvim-treesitter",
-	build = ":TSUpdate",
-	config = function()
-		local configs = require("nvim-treesitter.configs")
+  "nvim-treesitter/nvim-treesitter",
+  build = ":TSUpdate",
+  config = function()
+    local configs = require("nvim-treesitter.configs")
 
-		configs.setup({
-			ensure_installed = {
-				"cpp",
-				"javascript",
-				"typescript",
-				"tsx",
-				"lua",
-				"rust",
-				"go",
-				"css",
-				"json",
-				"html",
-			},
-			highlight = { enable = true },
-			indent = { enable = true },
+    configs.setup({
+      ensure_installed = {
+        "cpp",
+        "javascript",
+        "typescript",
+        "tsx",
+        "lua",
+        "rust",
+        "go",
+        "css",
+        "json",
+        "html",
+      },
+      highlight = { enable = true },
+      indent = { enable = true },
       -- autotag html auto close plugin
-			autotag = {
-				enable = true,
-			},
-		})
-	end,
+      autotag = {
+        enable = true,
+      },
+    })
+  end,
 }
