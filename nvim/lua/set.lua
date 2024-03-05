2b75f773a3756db9490ddf5d9952bc6ed4dcf8c0
Kirat Sidhu <kirat.sidhu.sw@gmail.com>
2024-03-04 19:47:26 -0800

    Add comments and prettier config
    - Format and add comments to set.lua
    - Add a preferred prettier config file to use in new projects

--- nvim/lua/set.lua
+++ nvim/lua/set.lua
@@ -1,5 +1,4 @@
 vim.g.mapleader = " "
-vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 vim.opt.nu = true
 vim.opt.relativenumber = true
 
@@ -26,25 +25,28 @@ vim.opt.scrolloff = 8
 -- Highlight line cursor is on
 vim.opt.cursorline = true
 
+-- Open file explorer
+vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+
 -- Diagnostic keymaps
-vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = 'Go to previous [D]iagnostic message' })
-vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next [D]iagnostic message' })
+vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, { desc = "Go to previous [D]iagnostic message" })
+vim.keymap.set("n", "]d", vim.diagnostic.goto_next, { desc = "Go to next [D]iagnostic message" })
 
 vim.opt.updatetime = 50
 vim.opt.backspace = { "start", "eol", "indent" }
 
 -- Turn off paste mode when leaving insert
 vim.api.nvim_create_autocmd("InsertLeave", {
-	pattern = "*",
-	command = "set nopaste",
+  pattern = "*",
+  command = "set nopaste",
 })
 
 -- Highlight when yanking text
 -- Pulled from kickstart.nvim (https://github.com/nvim-lua/kickstart.nvim/blob/master/init.lua)
 vim.api.nvim_create_autocmd("TextYankPost", {
-	desc = "Highlight when yanking text",
-	group = vim.api.nvim_create_augroup("highlight-yank", { clear = true }),
-	callback = function()
-		vim.highlight.on_yank()
-	end,
+  desc = "Highlight when yanking text",
+  group = vim.api.nvim_create_augroup("highlight-yank", { clear = true }),
+  callback = function()
+    vim.highlight.on_yank()
+  end,
 })
