45b85600c735633ddbfccd4cc56d8df685327ee2
Kirat Sidhu <kirat.sidhu.sw@gmail.com>
2024-03-07 22:31:39 -0800

    Update keybinds
    - Update keybinds to switch vim windows more easily
    - Update diagnostic keybind to avoid clashing window switching

--- nvim/lua/set.lua
+++ nvim/lua/set.lua
@@ -22,6 +22,12 @@ vim.opt.termguicolors = true
 
 vim.opt.scrolloff = 8
 
+-- Make pane switching more "vim-like"
+vim.keymap.set("n", "<C-h>", "<C-w><C-h>", { desc = "Move focus to the left window" })
+vim.keymap.set("n", "<C-l>", "<C-w><C-l>", { desc = "Move focus to the right window" })
+vim.keymap.set("n", "<C-j>", "<C-w><C-j>", { desc = "Move focus to the lower window" })
+vim.keymap.set("n", "<C-k>", "<C-w><C-k>", { desc = "Move focus to the upper window" })
+
 -- New Tab :D
 vim.keymap.set("n", "te", ":tabedit")
 vim.keymap.set("n", "<tab>", ":tabnext<Return>")
