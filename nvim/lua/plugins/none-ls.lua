cf2c148074f146921c946152bd8eae544caa52a4
Kirat Sidhu <kirat.sidhu.sw@gmail.com>
2024-03-20 15:38:05 -0700

    Update to prettierd
    - prettierd is faster and checks for local prettier config

--- nvim/lua/plugins/none-ls.lua
+++ nvim/lua/plugins/none-ls.lua
@@ -7,7 +7,7 @@ return {
     null_ls.setup({
       sources = {
         null_ls.builtins.formatting.stylua,
-        null_ls.builtins.formatting.prettier,
+        null_ls.builtins.formatting.prettierd,
         null_ls.builtins.formatting.gofumpt,
         null_ls.builtins.formatting.goimports_reviser,
         require("none-ls.diagnostics.eslint_d"),
