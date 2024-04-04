c2d2a4a208b0239e64468fa3a35bd5abc78bd52c
Kirat Sidhu <kirat.sidhu.sw@gmail.com>
2024-04-04 15:34:29 -0700

    Add diagnostics back to lualine
    - Custom diffs don't overwrite the existing option
    - It overwrote all the options
    - Add diagnostics back in
    - Exclude branch as don't need that in lualine

--- nvim/lua/plugins/lualine.lua
+++ nvim/lua/plugins/lualine.lua
@@ -1,84 +1,85 @@
 return {
-  "nvim-lualine/lualine.nvim",
-  config = function()
-    -- I just wanted to trim the mode name length
-    local mode_map = {
-      ["n"] = "N",
-      ["no"] = "O·P",
-      ["nov"] = "O·P",
-      ["noV"] = "O·P",
-      ["no\22"] = "O·P",
-      ["niI"] = "N·I",
-      ["niR"] = "N·R",
-      ["niV"] = "N",
-      ["nt"] = "N·T",
-      ["v"] = "V",
-      ["vs"] = "V",
-      ["V"] = "V·L",
-      ["Vs"] = "V·L",
-      ["\22"] = "V·B",
-      ["\22s"] = "V·B",
-      ["s"] = "S",
-      ["S"] = "S·L",
-      ["\19"] = "S·B",
-      ["i"] = "I",
-      ["ic"] = "I·C",
-      ["ix"] = "I·X",
-      ["R"] = "R",
-      ["Rc"] = "R·C",
-      ["Rx"] = "R·X",
-      ["Rv"] = "V·R",
-      ["Rvc"] = "RVC",
-      ["Rvx"] = "RVX",
-      ["c"] = "C",
-      ["cv"] = "EX",
-      ["ce"] = "EX",
-      ["r"] = "R",
-      ["rm"] = "M",
-      ["r?"] = "C",
-      ["!"] = "SH",
-      ["t"] = "T",
-    }
+	"nvim-lualine/lualine.nvim",
+	config = function()
+		-- I just wanted to trim the mode name length
+		local mode_map = {
+			["n"] = "N",
+			["no"] = "O·P",
+			["nov"] = "O·P",
+			["noV"] = "O·P",
+			["no\22"] = "O·P",
+			["niI"] = "N·I",
+			["niR"] = "N·R",
+			["niV"] = "N",
+			["nt"] = "N·T",
+			["v"] = "V",
+			["vs"] = "V",
+			["V"] = "V·L",
+			["Vs"] = "V·L",
+			["\22"] = "V·B",
+			["\22s"] = "V·B",
+			["s"] = "S",
+			["S"] = "S·L",
+			["\19"] = "S·B",
+			["i"] = "I",
+			["ic"] = "I·C",
+			["ix"] = "I·X",
+			["R"] = "R",
+			["Rc"] = "R·C",
+			["Rx"] = "R·X",
+			["Rv"] = "V·R",
+			["Rvc"] = "RVC",
+			["Rvx"] = "RVX",
+			["c"] = "C",
+			["cv"] = "EX",
+			["ce"] = "EX",
+			["r"] = "R",
+			["rm"] = "M",
+			["r?"] = "C",
+			["!"] = "SH",
+			["t"] = "T",
+		}
 
-    local function modes()
-      return mode_map[vim.api.nvim_get_mode().mode] or "__"
-    end
+		local function modes()
+			return mode_map[vim.api.nvim_get_mode().mode] or "__"
+		end
 
-    local icons = {
-      git = {
-        added = " ",
-        modified = " ",
-        removed = " ",
-      },
-    }
+		local icons = {
+			git = {
+				added = " ",
+				modified = " ",
+				removed = " ",
+			},
+		}
 
-    require("lualine").setup({
-      options = {
-        theme = "onedark",
-      },
-      sections = {
-        lualine_a = { modes },
-        lualine_b = {
-          {
-            "diff",
-            symbols = {
-              added = icons.git.added,
-              modified = icons.git.modified,
-              removed = icons.git.removed,
-            },
-            source = function()
-              local gitsigns = vim.b.gitsigns_status_dict
-              if gitsigns then
-                return {
-                  added = gitsigns.added,
-                  modified = gitsigns.changed,
-                  removed = gitsigns.removed,
-                }
-              end
-            end,
-          },
-        },
-      },
-    })
-  end,
+		require("lualine").setup({
+			options = {
+				theme = "onedark",
+			},
+			sections = {
+				lualine_a = { modes },
+				lualine_b = {
+					{
+						"diff",
+						symbols = {
+							added = icons.git.added,
+							modified = icons.git.modified,
+							removed = icons.git.removed,
+						},
+						source = function()
+							local gitsigns = vim.b.gitsigns_status_dict
+							if gitsigns then
+								return {
+									added = gitsigns.added,
+									modified = gitsigns.changed,
+									removed = gitsigns.removed,
+								}
+							end
+						end,
+					},
+					"diagnostics",
+				},
+			},
+		})
+	end,
 }
