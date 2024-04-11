5ced6e71e80f683aaac4b84462eb759849a59fcc
Kirat Sidhu <kirat.sidhu.sw@gmail.com>
2024-04-10 21:29:57 -0700

    Some needed cleanup
    - Add an organize imports to remove unused imports in TS files
    - Fix checkhealth tmux error
    - cat

--- nvim/lua/plugins/alpha.lua
+++ nvim/lua/plugins/alpha.lua
@@ -9,15 +9,15 @@ return {
     local dashboard = require("alpha.themes.startify")
 
     dashboard.section.header.val = {
-      [[                                  __]],
-      [[     ___     ___    ___   __  __ /\_\    ___ ___]],
-      [[    / _ `\  / __`\ / __`\/\ \/\ \\/\ \  / __` __`\]],
-      [[   /\ \/\ \/\  __//\ \_\ \ \ \_/ |\ \ \/\ \/\ \/\ \]],
-      [[   \ \_\ \_\ \____\ \____/\ \___/  \ \_\ \_\ \_\ \_\]],
-      [[    \/_/\/_/\/____/\/___/  \/__/    \/_/\/_/\/_/\/_/]],
+      [[                        ]],
+      [[         ／l、          ]],
+      [[        （ﾟ､ ｡ ７       ]],
+      [[         l  ~ヽ         ]],
+      [[         じしf_,)ノ     ]],
+      [[                        ]],
     }
 
-    dashboard.opts.opts.margin = 45
+    dashboard.opts.opts.margin = 55
     alpha.setup(dashboard.opts)
   end,
 }
