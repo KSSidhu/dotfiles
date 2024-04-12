e6629e3e83e42971819473a05ff9b558e0d33755
Gurkirat Sidhu <gurkirat@disco.co>
2024-04-12 14:19:05 +0000

    Fix missing eslint config file warning

--- nvim/lua/plugins/none-ls.lua
+++ nvim/lua/plugins/none-ls.lua
@@ -3,14 +3,20 @@ return {
   dependencies = { "nvimtools/none-ls-extras.nvim" },
   config = function()
     local null_ls = require("null-ls")
+    local null_ls_utils = require("null-ls.utils")
 
     null_ls.setup({
+      root_dir = null_ls_utils.root_pattern("package.json"),
       sources = {
         null_ls.builtins.formatting.stylua,
         null_ls.builtins.formatting.prettierd,
         null_ls.builtins.formatting.gofumpt,
         null_ls.builtins.formatting.goimports_reviser,
-        require("none-ls.diagnostics.eslint_d"),
+        require("none-ls.diagnostics.eslint_d").with({
+          condition = function(utils)
+            return utils.root_has_file({ ".eslintrc.js", ".eslintrc.cjs" }) -- only enable if root has .eslintrc.js or .eslintrc.cjs
+          end,
+        }),
       },
     })
 
