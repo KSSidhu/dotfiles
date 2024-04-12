566a1910e68177d52452b201755b3ddf0e38e1cc
Kirat Sidhu <kirat.sidhu.sw@gmail.com>
2024-04-11 21:34:55 -0700

    Remove unused and add oil.nvim
    - Replace netrw with oil.nvim
    - Remove autotag didn't work very well
    - Remove commentary, also didn't use very much

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -1,33 +1,28 @@
 return {
-	{
-		"nvim-treesitter/nvim-treesitter",
-		build = ":TSUpdate",
-		config = function()
-			local configs = require("nvim-treesitter.configs")
+  "nvim-treesitter/nvim-treesitter",
+  build = ":TSUpdate",
+  config = function()
+    local configs = require("nvim-treesitter.configs")
 
-			configs.setup({
-				ensure_installed = {
-					"cpp",
-					"javascript",
-					"typescript",
-					"tsx",
-					"lua",
-					"rust",
-					"go",
-					"css",
-					"json",
-					"html",
-				},
-				highlight = { enable = true },
-				indent = { enable = true },
-				-- autotag html auto close plugin
-				autotag = {
-					enable = true,
-				},
-			})
-		end,
-	},
-	{
-		"tpope/vim-commentary",
-	},
+    configs.setup({
+      ensure_installed = {
+        "cpp",
+        "javascript",
+        "typescript",
+        "tsx",
+        "lua",
+        "rust",
+        "go",
+        "css",
+        "json",
+        "html",
+      },
+      highlight = { enable = true },
+      indent = { enable = true },
+      -- autotag html auto close plugin
+      autotag = {
+        enable = true,
+      },
+    })
+  end,
 }
