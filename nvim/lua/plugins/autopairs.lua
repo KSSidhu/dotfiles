4638137abe117e92e0ee774b32b8b37b3cc2b544
Kirat Sidhu <kirat.sidhu.sw@gmail.com>
2024-04-12 16:45:58 -0700

    Add autotag

--- nvim/lua/plugins/autopairs.lua
+++ nvim/lua/plugins/autopairs.lua
@@ -1,4 +1,9 @@
 return {
-  "windwp/nvim-autopairs",
-  opts = {}, -- this is equalent to setup({}) function
+  {
+    "windwp/nvim-autopairs",
+    opts = {}, -- this is equalent to setup({}) function
+  },
+  {
+    "windwp/nvim-ts-autotag",
+  },
 }
