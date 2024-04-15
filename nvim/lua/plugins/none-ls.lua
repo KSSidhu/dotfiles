116dc9830e827f81401973566e8360581860ad0a
Kirat Sidhu <kirat.sidhu.sw@gmail.com>
2024-04-14 17:24:03 -0700

    Fix formatting

--- nvim/lua/plugins/none-ls.lua
+++ nvim/lua/plugins/none-ls.lua
@@ -1,25 +1,25 @@
 return {
-	"nvimtools/none-ls.nvim",
-	dependencies = { "nvimtools/none-ls-extras.nvim" },
-	config = function()
-		local null_ls = require("null-ls")
-		local null_ls_utils = require("null-ls.utils")
+  "nvimtools/none-ls.nvim",
+  dependencies = { "nvimtools/none-ls-extras.nvim" },
+  config = function()
+    local null_ls = require("null-ls")
+    local null_ls_utils = require("null-ls.utils")
 
-		null_ls.setup({
-			root_dir = null_ls_utils.root_pattern(".git", "package.json"),
-			sources = {
-				null_ls.builtins.formatting.stylua,
-				null_ls.builtins.formatting.prettierd,
-				null_ls.builtins.formatting.gofumpt,
-				null_ls.builtins.formatting.goimports_reviser,
-				require("none-ls.diagnostics.eslint_d").with({
-					condition = function(utils)
-						return utils.root_has_file({ ".eslintrc.js", ".eslintrc.cjs" }) -- only enable if root has .eslintrc.js or .eslintrc.cjs
-					end,
-				}),
-			},
-		})
+    null_ls.setup({
+      root_dir = null_ls_utils.root_pattern(".git", "package.json"),
+      sources = {
+        null_ls.builtins.formatting.stylua,
+        null_ls.builtins.formatting.prettierd,
+        null_ls.builtins.formatting.gofumpt,
+        null_ls.builtins.formatting.goimports_reviser,
+        require("none-ls.diagnostics.eslint_d").with({
+          condition = function(utils)
+            return utils.root_has_file({ ".eslintrc.js", ".eslintrc.cjs" }) -- only enable if root has .eslintrc.js or .eslintrc.cjs
+          end,
+        }),
+      },
+    })
 
-		vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
-	end,
+    vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
+  end,
 }
