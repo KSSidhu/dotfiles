6bdab41f54f18a9af12acff87b68de38195f383b
Kirat Sidhu <kirat.sidhu.sw@gmail.com>
2024-05-17 21:01:56 -0700

    Add completion icons

--- nvim/lua/plugins/completions.lua
+++ nvim/lua/plugins/completions.lua
@@ -10,8 +10,12 @@ return {
 	},
 	{
 		"hrsh7th/nvim-cmp",
+		dependencies = {
+			"onsails/lspkind.nvim",
+		},
 		config = function()
 			local cmp = require("cmp")
+			local lspkind = require("lspkind")
 			require("luasnip.loaders.from_snipmate").lazy_load()
 
 			cmp.setup({
@@ -24,6 +28,18 @@ return {
 				window = {
 					completion = cmp.config.window.bordered(),
 					documentation = cmp.config.window.bordered(),
+				},
+				formatting = {
+					format = lspkind.cmp_format({
+						mode = "symbol_text", -- show only symbol annotations
+						maxwidth = 50, -- prevent the popup from showing more than provided characters
+						ellipsis_char = "...", -- when popup menu exceed maxwidth, show truncated as ellipsis
+						show_labelDetails = true, -- show labelDetails in menu.
+
+						before = function(_, vim_item)
+							return vim_item
+						end,
+					}),
 				},
 				mapping = cmp.mapping.preset.insert({
 					["<C-b>"] = cmp.mapping.scroll_docs(-4),
