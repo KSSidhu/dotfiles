65a6c32f6c6c468f26ab4bd75bf18e6f799d4e2e
Kirat Sidhu <kirat.sidhu.sw@gmail.com>
2024-07-09 21:18:17 -0700

    Improve diagnostics text
    - Annoyed with duplicated diagnostics
    - Use a plugin to show diagnostics nicely

--- nvim/lua/plugins/linting.lua
+++ nvim/lua/plugins/linting.lua
@@ -1,23 +1,36 @@
 return {
-	"mfussenegger/nvim-lint",
-	event = {
-		"BufReadPre",
-		"BufNewFile",
-	},
-	config = function()
-		require("lint").linters_by_ft = {
-			javascript = { "eslint_d" },
-			typescript = { "eslint_d" },
-			javascriptreact = { "eslint_d" },
-			typescriptreact = { "eslint_d" },
-		}
+	{
+		"mfussenegger/nvim-lint",
+		event = {
+			"BufReadPre",
+			"BufNewFile",
+		},
+		config = function()
+			require("lint").linters_by_ft = {
+				javascript = { "eslint_d" },
+				typescript = { "eslint_d" },
+				javascriptreact = { "eslint_d" },
+				typescriptreact = { "eslint_d" },
+			}
 
-		vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost", "InsertLeave", "TextChanged" }, {
-			callback = function()
-				-- try_lint without arguments runs the linters defined in `linters_by_ft`
-				-- for the current filetype
-				require("lint").try_lint()
-			end,
-		})
-	end,
+			vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost", "InsertLeave", "TextChanged" }, {
+				callback = function()
+					-- try_lint without arguments runs the linters defined in `linters_by_ft`
+					-- for the current filetype
+					require("lint").try_lint()
+				end,
+			})
+		end,
+	},
+	{
+		"sontungexpt/better-diagnostic-virtual-text",
+		config = function()
+			require("better-diagnostic-virtual-text").setup({
+				inline = false,
+			})
+
+			-- Disable default diagnostics to avoid conflicts
+			vim.diagnostic.config({ virtual_text = false })
+		end,
+	},
 }
