92a9c3112143da33337e306e5dddf15144d69cfe
Kirat Sidhu <kirat.sidhu.sw@gmail.com>
2025-08-02 17:07:48 -0700

    Remove unneeded plugins and empty configs
    - Remove ufo-fold - wasn't really using them
    - Remove empty config option from theme
    - Add vim-coach for which helper

--- nvim/.config/nvim/lua/plugins/navigation.lua
+++ nvim/.config/nvim/lua/plugins/navigation.lua
@@ -57,5 +57,16 @@ return {
 			require("neoscroll").setup({})
 		end,
 	},
-	{ "kevinhwang91/nvim-ufo"},
+	{
+		"shahshlok/vim-coach.nvim",
+		dependencies = {
+			"folke/snacks.nvim",
+		},
+		config = function()
+			require("vim-coach").setup()
+		end,
+		keys = {
+			{ "<leader>?", "<cmd>VimCoach<cr>", desc = "Vim Coach" },
+		},
+	},
 }
