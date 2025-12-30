996e3e90948bd32f492b55e3c87fa10d2dbe1fe8
Kirat Sidhu <kirat.sidhu.sw@gmail.com>
2025-12-29 17:39:43 -0800

    Remove ts-translator
    - Recent upgrades broke the ts-translator
    - Found it was more annoying than helpful for myself

--- nvim/.config/nvim/lua/plugins/lsp-config.lua
+++ nvim/.config/nvim/lua/plugins/lsp-config.lua
@@ -137,7 +137,6 @@ return {
 			vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, {})
 
 			vim.lsp.handlers["textDocument/publishDiagnostics"] = function(err, result, ctx)
-				require("ts-error-translator").translate_diagnostics(err, result, ctx)
 				vim.lsp.diagnostic.on_publish_diagnostics(err, result, ctx)
 			end
 		end,
@@ -149,12 +148,6 @@ return {
 		config = function()
 			require("tiny-inline-diagnostic").setup()
 			vim.diagnostic.config({ virtual_text = false }) -- Disable Neovim's default virtual text diagnostics
-		end,
-	},
-	{
-		"dmmulroy/ts-error-translator.nvim",
-		config = function()
-			require("ts-error-translator").setup()
 		end,
 	},
 }
